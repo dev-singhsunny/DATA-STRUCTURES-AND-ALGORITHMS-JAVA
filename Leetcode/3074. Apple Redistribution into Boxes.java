b69fb756966775a657e1acf00ba5b1353e332dcb
dev-singhsunny <so39sunny@gmail.com>
2025-12-24 15:33:44 +0530

    Added solution for Leetcode 3074

---
+++ Leetcode/3074. Apple Redistribution into Boxes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+  public int minimumBoxes(int[] apple, int[] capacity) {
+    Arrays.sort(capacity);
+    int ans = 0, sum = 0;
+    for (int i = 0; i < apple.length; i++)
+      sum += apple[i];
+    for (int i = capacity.length - 1; i >= 0; i--) {
+      sum -= capacity[i];
+      ans++;
+      if (sum <= 0)
+        return ans;
+    }
+    return ans;
+  }
+}
