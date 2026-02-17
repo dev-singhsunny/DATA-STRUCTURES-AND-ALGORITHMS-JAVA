31a3178c2a95932a3eaa5bfcb35f7d5da5b10d86
dev-singhsunny <so39sunny@gmail.com>
2026-02-17 14:28:04 +0530

    fix code

--- Leetcode/190. Reverse Bits.java
+++ Leetcode/190. Reverse Bits.java
@@ -1,10 +1,12 @@
-class Solution { public int reverseBits(int n) { if(n == 0) return 0;
-int rev = 0;
-for(
-  int i = 0;
-i < 32;
-i + +
-) { rev =(rev << 1) |(n & 1);
-n = n >> 1;
-} return rev;
-} }
+class Solution {
+  public int reverseBits(int n) {
+    if (n == 0)
+      return 0;
+    int rev = 0;
+    for (int i = 0; i < 32; i++) {
+      rev = (rev << 1) | (n & 1);
+      n = n >> 1;
+    }
+    return rev;
+  }
+}
