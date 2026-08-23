0d4244e30efdfa0e2ed5e39740247815444f6808
dev-singhsunny <so39sunny@gmail.com>
2026-08-23 23:50:37 +0530

    Added solution for Leetcode4030

---
+++ Leetcode/4030. Check ASCII Palindromic.java
@@ -0,0 +1,22 @@
+class Solution {
+  public boolean isPalindromic(String s) {
+    StringBuilder binary = new StringBuilder();
+    for (char ch : s.toCharArray()) {
+      String b = Integer.toBinaryString(ch);
+      while (b.length() < 8) {
+        b = "0" + b;
+      }
+      binary.append(b);
+    }
+    int left = 0;
+    int right = binary.length() - 1;
+    while (left < right) {
+      if (binary.charAt(left) != binary.charAt(right)) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+    return true;
+  }
+}
